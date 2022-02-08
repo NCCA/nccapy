09cf453561f8ca9ed650f632d86f324ac50fa954
jmacey <jmacey@bournemouth.ac.uk>
2022-02-08 21:25:00 +0000

    updated timer class to add new methods

--- timer/Timer.py
+++ timer/Timer.py
@@ -2,23 +2,69 @@
 from __future__ import print_function
 from datetime import datetime
 
+"""
+Simple Timer class module, used to allow either simple elapsed time using
+with clause, or by using named timers with the start, get and reset methods 
+"""
+
+
+class TimerNotFoundException(Exception):
+    """Exception thrown if timer not in dictionary"""
+
+    pass
+
 
 class Timer:
-    """
-    Simple elapsed timer class can be used in a with clause or as stand alone named timers
-    """
+    """Simple elapsed timer class can be used in a with clause or as stand alone named timers."""
+
+    timers = {}
+    """this is a class attribute used by the class methods"""
 
     def __init__(self):
+        """Init method does nothing."""
         pass
 
     def __enter__(self):
+        """Start a timer if using with clause."""
         self.start = datetime.now()
 
     def __exit__(self, type, value, traceback):
+        """End timer and report if using with clause."""
         end = datetime.now()
         delta = end - self.start
         print("Elapsed time {} ms".format(int(delta.total_seconds() * 1000)))
         return False
+
+    @classmethod
+    def add_timer(cls, name: str):
+        """Add timer to the class timer dictionary"""
+        Timer.timers[name] = None
+
+    @classmethod
+    def start_timer(cls, name: str):
+        """Start / reset the named timer or throw exception."""
+        if Timer.timers.get(name) is None:
+            Timer.timers[name] = datetime.now()
+        else:
+            raise TimerNotFoundException
+
+    @classmethod
+    def remove_timer(cls, name: str):
+        """Remove the named timer or throw exception."""
+        if Timer.timers.get(name) is not None:
+            del Timer.timers[name]
+        else:
+            raise TimerNotFoundException
+
+    @classmethod
+    def get_elapsed_ms(cls, name: str) -> int:
+        """Get elapsed time as ms for named timer"""
+        if Timer.timers.get(name) is not None:
+            end = datetime.now()
+            delta = end - Timer.timers[name]
+            return int(delta.total_seconds() * 1000)
+        else:
+            raise TimerNotFoundException
 
 
 if __name__ == "__main__":
@@ -27,3 +73,13 @@ if __name__ == "__main__":
     print("Running Timer Test")
     with Timer() as t:
         time.sleep(1)
+    Timer.add_timer("TestTimer")
+    Timer.start_timer("TestTimer")
+    for i in range(0, 5):
+        time.sleep(1)
+        print("Getting elapsed {} ms".format(Timer.get_elapsed_ms("TestTimer")))
+    Timer.remove_timer("TestTimer")
+    try:
+        Timer.get_elapsed_ms("TestTimer")
+    except TimerNotFoundException:
+        print("Test timer doesn't exist")
